37198b049478cad9558e737a2164f6658d216a81
prplV <maseeeeeeeed@gmail.com>
2023-10-18 17:32:04 +0300

    add files

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+#[path = "meta.rs"] mod meta;
+
+use std::io;
+use crate::meta::Calc;
+use crate::meta::OperationType;
+
+fn main() {
+    let calculator: Calc = Calc::new();
+    let mut first_number = String::new();
+    let mut second_number = String::new();
+
+    io::stdin().read_line(&mut first_number).expect("readline 1 number error !");
+    io::stdin().read_line(&mut second_number).expect("readline 2 number error !");
+
+    let first_number :i8 = first_number.trim().parse().expect("parsing 1 num error ! (it's not a number)");
+    let second_number :i8 = second_number.trim().parse().expect("parsing 2 num error ! (it's not a number)");
+    
+    let mut op = String::new();
+    io::stdin().read_line(&mut op).expect("readline operation error !");
+    let op:OperationType = match op.trim(){
+        "+" => OperationType::Adddition,
+        "-" => OperationType::Substraction,
+        "*" => OperationType::Multipling,
+        "/" => OperationType::Dividing,
+        _ => panic!("That's not a math operation"),
+    };
+
+    if !calculator.verify_number(first_number){
+        panic!("1st number isn't in (1..=10) range");
+    }
+    if !calculator.verify_number(second_number){
+        panic!("2nd number isn't in (1..=10) range");
+    }
+
+    // calculating and converting
+    let answer = calculator.convert_to_roman_nums(calculator.calculate(first_number, second_number, op));
+    
+    println!("Roman_nums format output is {}", answer);
+}
---
+++ src/meta.rs
@@ -0,0 +1,80 @@
+pub enum OperationType{
+    Adddition,
+    Substraction,
+    Multipling,
+    Dividing,
+}
+
+pub struct Calc{
+    roman_nums_map: Vec<(String, i8)>
+}
+
+impl Calc{
+    
+    pub fn new() -> Calc{
+        let mut temp_map: Vec<(String, i8)> = Vec::new();
+
+        temp_map.push((String::from("C"), 100));
+        temp_map.push((String::from("L"), 50));
+        temp_map.push((String::from("X"), 10));
+        temp_map.push((String::from("V"), 5));
+        temp_map.push((String::from("I"), 1));
+
+        Calc{
+            roman_nums_map: temp_map,
+        }
+    }
+    
+    pub fn calculate(&self, f_number: i8, s_number: i8, operation: OperationType) -> i8 {
+        match operation{
+            OperationType::Adddition => f_number + s_number,
+            OperationType::Substraction => f_number - s_number,
+            OperationType::Multipling => f_number * s_number,
+            OperationType::Dividing => f_number / s_number
+        }
+    }
+    
+    pub fn verify_number(&self, num: i8) -> bool {
+        if (num < 1) || (num > 10) {
+            return false;
+        }
+        true
+    }
+
+    pub fn convert_to_roman_nums(&self, mut witness: i8) -> String{
+        let mut roman_output = String::new();
+
+        if witness == 0{
+            return "null".to_string();
+        }
+
+        for (key, value) in &self.roman_nums_map {
+            if (value == &100) || (value == &50) {
+                while (witness - value) >= 0 {
+                    witness -= value;
+                    roman_output = roman_output + key;
+                }
+                // XC XL
+                if (value - witness) <= 10 {
+                    roman_output = roman_output + "X" + key;
+                    witness = witness + 10 - value;
+                }
+                // XC XL
+            }
+            else {
+                while (witness - value) >= 0 {
+                    witness -= value;
+                    roman_output = roman_output + key;
+                }
+                // IX IV 
+                if ((value - witness) == 1) && (witness != 0)
+                {
+                    witness = witness + 1 - value;
+                    roman_output = roman_output + "I" + key;
+                }
+                //  IX IV 
+            }
+        }
+        return roman_output;
+    }
+}
